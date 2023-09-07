398d6906b71ebb1ec5be139fa50665b7e93ad7a6
fernando <geramxcervan@gmail.com>
2023-09-07 14:55:25 -0600

    first commit

---
+++ facerecognition/app/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from app.models import FaceRecognition
+
+class FaceRecognitionform(forms.ModelForm):
+    
+    class Meta:
+        model = FaceRecognition
+        fields =['image']
+        
